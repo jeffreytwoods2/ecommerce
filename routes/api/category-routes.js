4a7c68557fda7ff85c9a37c9cfb0fcc79b4de23d
GnomeSaiyan <69124994+jeffreytwoods2@users.noreply.github.com>
2021-01-09 20:57:55 -0700

    Added put route to update categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -40,8 +40,21 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const catData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!catData[0]) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
