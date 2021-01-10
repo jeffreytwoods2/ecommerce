f4f7d753e4b0c789a786b1e29709c16db107ca9a
GnomeSaiyan <69124994+jeffreytwoods2@users.noreply.github.com>
2021-01-09 21:18:54 -0700

    Added tag CRUD routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -32,7 +32,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   try {
     const catData = await Category.create(req.body);
-    res.status(200).json(err)
+    res.status(200).json(catData)
   } catch (err) {
     res.status(400).json(err)
   }
