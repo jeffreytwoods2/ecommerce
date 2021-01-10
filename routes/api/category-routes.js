f45bcf5a8b727af3039ce1ceeb42d13f5c45e8c3
GnomeSaiyan <69124994+jeffreytwoods2@users.noreply.github.com>
2021-01-09 20:44:44 -0700

    Create get routes for Category data

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,16 +1,34 @@
 const router = require('express').Router();
+const sequelize = require('../../config.connection');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const catData = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(catData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const catData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+    if (!catData) {
+      res.status(404).json({ message: "No category found with that id!" })
+    };
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.post('/', (req, res) => {
